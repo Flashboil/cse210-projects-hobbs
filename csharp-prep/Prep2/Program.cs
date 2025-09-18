f12459086c3075e1587b6bd0cfba50e55725bbf7
Jonathan-Hobbs <jonathan_hobbs@byui.edu>
2025-09-18 12:04:51 -0600

    completed prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string gradeText = Console.ReadLine();
+        int grade = int.Parse(gradeText);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Nice work!");
+        }
+        else
+        {
+            Console.WriteLine("No quite there. Keep trying!");
+        }
+
     }
 }
