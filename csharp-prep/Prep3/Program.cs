5022bf952719f5f6b94898dadc3c0be0a399c893
Jonathan-Hobbs <jonathan_hobbs@byui.edu>
2025-09-25 14:03:53 -0600

    completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        int num_guesses = 0;
+
+        Console.WriteLine("Guess a number between 1 and 100.");
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low!");
+            }
+
+            num_guesses += 1;
+
+        } while (guess != number);
+
+        Console.WriteLine($"You got it! The number was {number}");
+        Console.WriteLine($"It took you {num_guesses} guesses.");
+
     }
 }
