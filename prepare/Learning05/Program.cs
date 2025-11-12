3f12476a52ad02c72a32f2e1f892806cc440d7e6
Jonathan-Hobbs <jonathan_hobbs@byui.edu>
2025-11-11 17:21:54 -0700

    shpaes polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 4);
+        Rectangle rect = new Rectangle("blue", 3, 4);
+        Circle circle = new Circle("yellow", 5);
+
+        List<Shape> shapes = new List<Shape> { square, rect, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
